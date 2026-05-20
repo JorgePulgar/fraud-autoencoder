0059b625b569f581cff0f02355dea92ab5b1e26a
Jorge Pulgar <jpulgar111@gmail.com>
2026-05-20 17:33:03 +0200

    feat(baselines): add logistic regression baseline

--- src/baselines.py
+++ src/baselines.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.ensemble import IsolationForest
+from sklearn.linear_model import LogisticRegression
 
 
 class _IsoForestWrapper:
@@ -8,6 +9,24 @@ class _IsoForestWrapper:
 
     def score(self, X: np.ndarray) -> np.ndarray:
         return -self._model.decision_function(X)
+
+
+class _LogRegWrapper:
+    def __init__(self, model: LogisticRegression):
+        self._model = model
+
+    def score(self, X: np.ndarray) -> np.ndarray:
+        return self._model.predict_proba(X)[:, 1]
+
+
+def train_logistic_regression(
+    X_train_full: np.ndarray, y_train_full: np.ndarray
+) -> _LogRegWrapper:
+    lr = LogisticRegression(
+        class_weight="balanced", random_state=42, max_iter=1000
+    )
+    lr.fit(X_train_full, y_train_full)
+    return _LogRegWrapper(lr)
 
 
 def train_isolation_forest(X_train_legit: np.ndarray) -> _IsoForestWrapper:
